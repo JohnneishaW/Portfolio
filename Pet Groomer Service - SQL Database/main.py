9a1efc82c45b494782f01fc07a68e4acf935737c
Johnneisha White <johnneisha.white@gmail.com>
2023-07-02 16:57:59 -0500

    added pet and vaccination data creation code

--- Pet Groomer Service - SQL Database/main.py
+++ Pet Groomer Service - SQL Database/main.py
@@ -103,7 +103,7 @@ def create_shift(jobs):
     while counter<2:
         for j in jobs:
             Job.append(j)
-            print(j)
+            #print(j)
             shiftdate = date.fromisoformat(str(
             random.randrange(2019, 2024)) + '-' + str(random.randrange(10, 12))+ '-' + str(random.randrange(10, 28)))
             ShiftDate.append(shiftdate)
@@ -127,9 +127,70 @@ def create_shift(jobs):
         'ShiftEndTime': ShiftEndTime
     }
     df = pd.DataFrame(data)
-    print(df.head())
+    #print(df.head())
     return df
 
+def create_pet(petNames, petBreeds,petStatus):
+   #TO DO OWNER 
+    #owner = []
+    name = []
+    breeds = []
+    petType = []
+    age = []
+    birthday = []
+    weight = []
+    status = []
+
+
+    for n in petNames:
+        name.append(n)
+        selectedBreedandType = petBreeds.iloc[random.randrange(0, (petBreeds.shape[0]) - 1), :]
+        breeds.append(selectedBreedandType['Breed'])
+        petType.append(selectedBreedandType['Type'])
+        age.append(random.randrange(2,9))
+        birthday.append(date.fromisoformat(str(random.randrange(2019, 2024)) + '-' + str(random.randrange(10, 12))+ '-' + str(random.randrange(10, 28))))
+        weight.append(random.randrange(5,30))
+        status.append(petStatus[random.randrange(0, len(petStatus)-1)])
+                        
+
+    data = {
+        #'PetOwnerID': Job,
+        'Name': name,
+        'Breed': breeds,
+        'Age': age,
+        'Birthday': birthday,
+        'Weight': weight,
+        'Status': status,
+        'Type': petType
+    }
+    df = pd.DataFrame(data)
+    #print(df.head())
+    return df
+
+def create_vaccination(vacList):
+    name = []
+    expirDate = []
+    PetID = []
+
+    counter = 0
+
+    while counter<2:
+        for p in pet.index:
+        PetID.append(p)
+        name.append(vacList[random.randrange(0, len(vacList)-1)])
+        expirDate.append(date.fromisoformat(str(random.randrange(2000, 2022)) + '-' + str(random.randrange(10, 12))+ '-' + str(random.randrange(10, 28))))
+    counter+=1
+    
+    data = {
+        #'PetOwnerID': Job,
+        'Name': name,
+        'Expiration': expirDate,
+        'PetID': PetID
+    }
+
+    df = pd.DataFrame(data)
+    #print(df.head())
+    return df
 
 if __name__ == '__main__':
     # Load File Data Into Data Structure
@@ -142,8 +203,10 @@ if __name__ == '__main__':
     serviceNames = load_file_into_numpy_array('Service Names.txt', " \n")
     status = load_file_into_numpy_array('Status.txt', " \n")
     breeds = load_file_into_pandas_dataframe("Breeds.txt", ",")
+    vaccinations = load_file_into_numpy_array('Vaccinations.txt', ",")
 
     store = create_store(storeName, parse_address(address, storeName))
     service = create_service(serviceNames)
     shift = create_shift(jobFile)
-
+    pet = create_pet(petName, breeds,status)
+    vaccination = create_vaccination(vaccinations)
